a262e06fe9309d4928130d1eb8af70ddb7297fab
Anderson Ramalho <brunnowtf@gmail.com>
2021-08-26 19:14:00 -0300

    Adicionando validação para input vazio e adicionando novas tarefas

---
+++ script.js
@@ -0,0 +1,17 @@
+// Adicionando validação para campo de input vazio
+document.querySelector('#push').onclick = function() {
+  if(document.querySelector('#newtask input').value.length == 0) {
+    alert("Por favor, insira uma tarefa válida!")
+  } else {
+    // Adicionando uma nova tarefa
+    document.querySelector('#tasks').innerHTML 
+    += `<div class="task">
+          <span id="taskname">
+            ${document.querySelector('#newtask input').value}
+          </span>
+          <button class="delete">
+            <i class="far fa-trash-alt"></i>
+          </button>
+        </div>`
+  }
+}
