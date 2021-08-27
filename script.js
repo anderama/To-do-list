bc459675acdf429f0c4423c9a4d8513b69ad9f57
Anderson Ramalho <brunnowtf@gmail.com>
2021-08-27 19:56:19 -0300

    Projeto terminado e já rodando

--- script.js
+++ script.js
@@ -1,26 +1,43 @@
-// Adicionando validação para campo de input vazio
-document.querySelector('#push').onclick = function() {
-  if(document.querySelector('#newtask input').value.length == 0) {
-    alert("Por favor, insira uma tarefa válida!")
-  } else {
-    // Adicionando uma nova tarefa
-    document.querySelector('#tasks').innerHTML 
-    += `<div class="task">
-          <span id="taskname">
-            ${document.querySelector('#newtask input').value}
-          </span>
-          <button class="delete">
-            <i class="far fa-trash-alt"></i>
-          </button>
-        </div>
-    `;
+// Validando input vazio
+document.querySelector('#push').onclick = function(){
+  if(document.querySelector('#newtask input').value.length == 0){
+      alert("Please Enter a Task")
+  }
+  else{
+      // Adicionando uma nova tarefa
+      document.querySelector('#tasks').innerHTML += `
+          <div class="task">
+              <span id="taskname">
+                  ${document.querySelector('#newtask input').value}
+              </span>
+              <button class="delete">
+                  <i class="far fa-trash-alt"></i>
+              </button>
+          </div>
+      `;
 
-    // Deletando a tarefa criada
-    var currentTasks = document.querySelectorAll('.delete');
-    for(var i=0; i<currentTasks.length; i++) {
-      currentTasks[i].onclick = function() {
-        this.parentNode.remove();
+      // Deletando uma tarefa
+      var current_tasks = document.querySelectorAll(".delete");
+      for(var i=0; i<current_tasks.length; i++){
+          current_tasks[i].onclick = function(){
+              this.parentNode.remove();
+          }
       }
-    }
+
+      // Riscando tarefa concluída
+      var tasks = document.querySelectorAll(".task");
+      for(var i=0; i<tasks.length; i++){
+          tasks[i].onclick = function(){
+              this.classList.toggle('completed');
+          }
+      }
+
+      document.querySelector("#newtask input").value = "";
   }
 }
+
+  window.addEventListener("keyup", function(event) {
+  if (event.key == 'Enter') {
+    this.document.getElementById('push').click();
+  }
+});
